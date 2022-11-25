1dcb6912321b630b629718bd342051ae52df2dfd
Nico3117 <nicobaca17@gmail.com>
2022-11-25 17:08:06 +0100

    request api winner last gp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const requestOptions = React.useState({
+    method: 'GET',
+    redirect: 'follow'
+  });
+
+  const [winner, setWinner] = React.useState("");
+  
+  React.useEffect(() => {
+    fetch("https://ergast.com/api/f1/current/last/results.json", requestOptions)
+    .then((res) => { return res.json() })
+    .then((data) => { return setWinner(data.MRData.RaceTable.Races[0].Results[0].Driver.familyName) })
+    .catch((error) => { return console.log('error', error) });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>{winner}</h1>
+    </>
   );
 }
 
